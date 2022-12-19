bf264d7bd745ccd955400ade40f33c293c9fd7b6
Weronika Derkowska <weronika.derkowska@gmail.com>
2022-12-19 06:45:00 +0100

    update project

--- js/constants/projectList.js
+++ js/constants/projectList.js
@@ -1,5 +1,26 @@
 export const projectArray = [
     {
+        title: "Project Exam 2",
+        description: "We had a choice between creating a social media website and real life client project. Since I worked for company that runned Cantine for few customers I was always lacking tool for customers to order food for meetings. This project is work in progress on that solution.",
+        image: "img/pe2.png",
+        github: "https://github.com/Noroff-FEU-Assignments/project-exam-2-werodev",
+        preview: "https://coruscating-daffodil-18ec7e.netlify.app/",
+    },
+    {
+        title: "JavaScript Frameworks CA",
+        description: "This project was a chance to show overall we have learned about frameworks. We could choose between React and Next.js. I've picked 2nd one. I decided to make my daughters library to share her bookshelves with her friends. Unfortunatelly Heroku's Postgress stopped beeing aviable in free plan and I need to find other solution to deploy my Strapi. Project will be updated.",
+        image: "img/nop-01.png",
+        github: "https://github.com/Noroff-FEU-Assignments/js-frameworks-course-assignment-werodev",
+        preview: "",
+    },
+    {
+        title: "Semester Project 2",
+        description: "Our goal was to create an e-commerce website fallowing described architecture: Home Page with hero banner and featured products / Products Page with list of all products (each product display tile, price and image), serchbox for filtering / Products detail page / Cart-Basket Page / Admin section. I failed this one and since Postgress is not supported anymore for free - website have no API to connect to and as a result - nothing here to show.",
+        image: "img/nop-01.png",
+        github: "https://github.com/werodev/Semester_Project2",
+        preview: "",
+    },
+    {
         title: "HTML & CSS - Course Assignment: Mobile-responsive website",
         description: "Website for Re-cycle - company that offer enviroment friendly, guided cycling tours. Since their users are traveling it's important that website will be mobile responsive.",
         image: "img/recycle.jpg",
